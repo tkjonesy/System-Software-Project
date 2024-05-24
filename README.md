36d27e4ca86b408d46b0a5c224f5501bf68e25f6
Trever <tkjones123456@gmail.com>
2024-05-24 18:11:18 -0400

    Added logic for reading in an input file and adding the ISA to the PAS

--- README.md
+++ README.md
@@ -1,13 +1,31 @@
 # System Software Project Files
 
-This Repository contains the program files and build intructions for the System Software programming project. 
+This Repository contains the program files and build intructions for the System Software programming project. <br>
+
+### Contributers:
+
+Trever Jones<br>
+Jose Porta<br>
 
 ## Table of Contents
 
 - [Getting Started](#getting-started)
+- [Running The Code](running-the-code)
 
 
 ## Getting Started
 
 Clone the repository:<br>
 `git clone https://github.com/tkjonesy/System-Software-Project.git`
+
+
+## Getting Started
+
+Navigate to the folder: <br>
+`cd ...\System Software Project`
+
+Compile the code:<br>
+`gcc -o vm vm.c`
+
+Ensure input file is in the same directory then run the .exe passing the input file name:<br>
+`vm.exe <inputFileName>`
--- vm.c
+++ vm.c
@@ -1,7 +1,39 @@
+/* 
+COP3402 Programming Project
+Trever Jones
+Jose Porta
+*/
+
 #include <stdio.h>
 
-int main()
+int main(int argc, char *argv[])
 {
-    printf("Hello World");
+    int PAS[500] = {0};
+    int OP, L, M;
+    int index = 10;
+
+    FILE *file;
+    file = fopen(argv[1], "r");
+
+    // Checking for correct number of argumnets passed (Trever Jones)
+    if (argc != 2) {
+    printf("Please use correct command: %s <inputFileName>\n", argv[0]);
+    return 1;
+    }
+
+    if (file != NULL) {
+        while (fscanf(file, "%d %d %d", &OP, &L, &M) == 3) {
+            PAS[index] = OP;
+            PAS[index + 1] = L;
+            PAS[index + 2] = M;
+            index += 3;
+        }
+
+    }
+    // File name passed incorrectly as argumnent or file does not exsist (Trever Jones)
+    else {
+        printf("No file named %s found\n", argv[1]);
+    }
+
     return 0;
 }
