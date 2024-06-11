b1236729a5eaf70d92c03ab135d2b85e250593b0
Trever <tkjones123456@gmail.com>
2024-06-10 21:47:35 -0400

    Added logic to read input file from command line into dynamic array

--- lex.c
+++ lex.c
@@ -4,6 +4,9 @@ Trever Jones
 Jose Porta
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+
 // Max length macros (Jose Porta)
 #define MAX_ID_LEN 11
 #define MAX_NUM_LEN 5
@@ -56,4 +59,47 @@ typedef struct Token {
   char lexeme[MAX_ID_LEN + 1];
 } Token;
 
-int main() {}
+int main(int argc, char *argv[]) {
+  char *inputArr = NULL;
+  FILE *file;
+  file = fopen(argv[1], "r");
+
+  // Checking for correct number of argumnets passed (Trever Jones)
+  if (argc != 2) {
+    printf("Please use correct command: %s <inputFileName>\n", argv[0]);
+    return 1;
+  }
+
+  // Reading in input from input file (Trever Jones)
+  if (file != NULL) {
+
+    // Get the size of the file (Trever Jones)
+    fseek(file, 0, SEEK_END);
+    long fileSize = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    // Dynamically allocate input array (Trever Jones)
+    inputArr = (char *)malloc(fileSize + 1);
+    if (inputArr == NULL) {
+        printf("Memory allocation failed\n");
+        fclose(file);
+        return 1;
+    }
+
+    // Read in file contents (Trever Jones)
+    fread(inputArr, 1, fileSize, file);
+    inputArr[fileSize] = '\0';
+    fclose(file);
+  }
+
+  // File name passed incorrectly as argumnent or file does not exsist (Trever Jones)
+  else {
+    printf("No file named %s found\n", argv[1]);
+    return 1;
+  }
+
+  printf("Source Program:\n%s\n", inputArr);
+
+  free(inputArr);
+  return 0;
+}
